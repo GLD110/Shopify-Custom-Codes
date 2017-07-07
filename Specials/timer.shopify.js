f4b17561c2adb89b78e8af57c04634b0b537a46a
diana4545 <diana4545@outlook.com>
2017-07-07 16:22:46 +0200

    update

--- Specials/timer.shopify.js
+++ Specials/timer.shopify.js
@@ -1,4 +1,4 @@
 $("document").ready(function() {
-	$('#counter_container').append('<p>Your app is using jQuery version '+$.fn.jquery+'</p>');
+	$('#mytimer_container').append('<p>Your app is using jQuery version '+$.fn.jquery+'</p>');
 });
 
